4ab6395ebce80ee2be568448cadc37b6f2e7ea4a
AnushaKAntony <anushakantony@gmail.com>
2024-07-20 21:11:00 +0530

    Posted created

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const Cors = require("cors")
 const Jsonwebtoken = require("jsonwebtoken")
 const Bcrypt = require("bcrypt")
 const userModel=require("./models/users")
+const postModel = require("./models/posts")
 
 let app=Express()
 
@@ -11,6 +12,23 @@ app.use(Express.json())
 app.use(Cors())
 
 Mongoose.connect("mongodb+srv://anusha:anusha13@cluster0.hyxpaoy.mongodb.net/newblogDB?retryWrites=true&w=majority&appName=Cluster0")
+
+//create a post
+app.post("/create",async(req,res)=>{
+    let input=req.body
+
+    let token=req.headers.token
+    Jsonwebtoken.verify(token,"blogApp",async(error,decoded)=>{
+        if (decoded && decoded.email) {
+            let result=new postModel(input)
+            await result.save()
+            res.json({"status":"success"})
+        } else {
+            res.json({"status":"Invalid Authentication"})
+        }
+    })
+})
+
 
 //SignIn
 app.post("/signin",async(req,res)=>{
