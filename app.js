f6d04bbcf44765dbec0a102b1629778442f258d8
AnushaKAntony <anushakantony@gmail.com>
2024-07-20 13:30:04 +0530

    Signin completed

--- app.js
+++ app.js
@@ -12,7 +12,35 @@ app.use(Cors())
 
 Mongoose.connect("mongodb+srv://anusha:anusha13@cluster0.hyxpaoy.mongodb.net/newblogDB?retryWrites=true&w=majority&appName=Cluster0")
 
+//SignIn
+app.post("/signin",async(req,res)=>{
+      let input=req.body
+      let result=userModel.find({email:req.body.email}).then(
+        (items)=>{
+            if (items.length>0) {
+                const passwordValidator=Bcrypt.compareSync(req.body.password,items[0].password)
+                if (passwordValidator) {
+                    Jsonwebtoken.sign({email:req.body.email},"blogApp",{expiresIn:"1d"},
+                        (error,token)=>{
+                            if (error) {
+                                res.json({"status":"error","error":error})
+                            } else {
+                                res.json({"status":"success","token":token,"userId":items[0]._id})
+                            }
+                        })
 
+                } else {
+                    res.json({"status":"Incorrect password"})
+                }
+            } else {
+                res.json({"status":"Invalid email ID"})
+            }
+        }
+      )
+})
+
+
+//SignUp
 app.post("/signup",async(req,res)=>{
      let input=req.body
      let hashedPassword = Bcrypt.hashSync(req.body.password,10)
